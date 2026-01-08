72cc4eccee7b4c1093c7ee96dabb8a52e73d3a4a
Mankudteen <143701410+Mankudteen@users.noreply.github.com>
2026-01-08 14:32:30 +0700

    Update model.py

--- Project_1/Code_and_Experiments/Experiments/14dec/model.py
+++ Project_1/Code_and_Experiments/Experiments/14dec/model.py
@@ -3,30 +3,38 @@ import torch.nn as nn
 
 
 class ThaiDigitNet(nn.Module):
-def __init__(self):
-super().__init__()
-def block(i, o):
-return nn.Sequential(
-nn.Conv2d(i, o, 3, padding=1, bias=False),
-nn.BatchNorm2d(o),
-nn.LeakyReLU(0.1),
-nn.Conv2d(o, o, 3, padding=1, bias=False),
-nn.BatchNorm2d(o),
-nn.LeakyReLU(0.1),
-nn.MaxPool2d(2),
-nn.Dropout(0.25)
-)
-self.b1 = block(1, 32)
-self.b2 = block(32, 64)
-self.b3 = block(64, 128)
-self.b4 = block(128, 256)
-self.fc = nn.Sequential(
-nn.Flatten(),
-nn.Linear(256*6*6, 512, bias=False),
-nn.BatchNorm1d(512),
-nn.LeakyReLU(0.1),
-nn.Dropout(0.5),
-nn.Linear(512, 10)
-)
-def forward(self, x):
-return self.fc(self.b4(self.b3(self.b2(self.b1(x)))))
+    def __init__(self):
+        super().__init__()
+
+        def block(in_c, out_c):
+            return nn.Sequential(
+                nn.Conv2d(in_c, out_c, kernel_size=3, padding=1, bias=False),
+                nn.BatchNorm2d(out_c),
+                nn.LeakyReLU(0.1),
+                nn.Conv2d(out_c, out_c, kernel_size=3, padding=1, bias=False),
+                nn.BatchNorm2d(out_c),
+                nn.LeakyReLU(0.1),
+                nn.MaxPool2d(2),
+                nn.Dropout(0.25),
+            )
+
+        self.b1 = block(1, 32)
+        self.b2 = block(32, 64)
+        self.b3 = block(64, 128)
+        self.b4 = block(128, 256)
+
+        self.fc = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(256 * 6 * 6, 512, bias=False),
+            nn.BatchNorm1d(512),
+            nn.LeakyReLU(0.1),
+            nn.Dropout(0.5),
+            nn.Linear(512, 10),
+        )
+
+    def forward(self, x):
+        x = self.b1(x)
+        x = self.b2(x)
+        x = self.b3(x)
+        x = self.b4(x)
+        return self.fc(x)
